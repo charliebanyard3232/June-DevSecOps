21e491addfb7cc4efd2d41733353e49ee7072e76
charlie <charlie@cyberagoge.com>
2025-08-10 11:24:35 +0100

    update

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
+resource "aws_s3_bucket" "terraformbuckettodayjune" {
+  bucket = "terraformbuckettodayjune"
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraformbuckettodayjune"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
